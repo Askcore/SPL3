aad2722f281e2ab5d51a41318c3fe842b0b90d48
Askcore <171witb14@o365.htl-leoben.at>
2018-04-25 12:28:48 +0200

    Updated Version- Geschafft yay

--- zeiten.py
+++ zeiten.py
@@ -1,12 +1,19 @@
 # zeiten.py 
 
+import sys
+
 def zeitInSekunden(h, m, s):
     return int(s) + int(m)*60 + int(h)*3600
 
-beginnZeit = input("Beginnzeit: ")
-endeZeit = input("Endzeit: ")
+#beginnZeit = input("Beginnzeit: ")
+#endeZeit = input("Endzeit: ")
 
 # Zeit im Format HH:MM:SS
+
+p = sys.argv
+
+beginnZeit = p[1]
+endeZeit = p[2]
 
 beginn = beginnZeit.split(":")
 ende = endeZeit.split(":")
