58c10fd7ac29d0b403c44a885e9cd8c8501ed5fd
Askcore <171witb14@o365.htl-leoben.at>
2018-04-25 09:22:48 +0200

    Updated version-Versuche gerade die maximal und minimal anzahl zu machen

--- parkhaus.py
+++ parkhaus.py
@@ -6,10 +6,25 @@ h = input("Wie viele Haltestellen soll es geben? ")
 h = int(h)
 personen = 0
 
-print("Der Bus fährt los.")
+print("Der Bus fährt los.\n")
 
-for h in range (0,h+1):
-    print("Der Bus ist in Haltestelle",h,end="")
+for h in range (1,h+1):
+    print("Der Bus ist in Haltestelle ",h,".",sep="")
+    einstieg = input("Wie viele Personen steigen ein? ")
+    einstieg = int(einstieg)
+    if (einstieg < 60)
+        print("")
+    
+    
+    personen = personen + einstieg
+    ausstieg = input("Wie viele Personen steigen aus? ")
+    ausstieg = int(ausstieg)
+    
+    
+    
+    personen = personen - ausstieg
+    print("Es sind ",personen,"Personen im Bus.\n")
+
 
 
 
@@ -22,4 +37,4 @@ for h in range (0,h+1):
 
 
 print("Der Bus hat die Endstation erreicht.")
-print("Bitt alle aussteigen.")
+print("'Bitt alle aussteigen!'")
