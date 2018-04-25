d9fd71228c5a232e59c52dbcf5b909bd60d702e1
Askcore <171witb14@o365.htl-leoben.at>
2018-04-25 10:06:57 +0200

    Updated version-Eigentlich fertig :D

--- parkhaus.py
+++ parkhaus.py
@@ -5,36 +5,26 @@ print("Linienbus-Simulator")
 h = input("Wie viele Haltestellen soll es geben? ")
 h = int(h)
 personen = 0
+i = 0
 
 print("Der Bus fährt los.\n")
 
-for h in range (1,h+1):
+while (i < h):
+    i = i+1
     print("Der Bus ist in Haltestelle ",h,".",sep="")
     einstieg = input("Wie viele Personen steigen ein? ")
     einstieg = int(einstieg)
-    if (einstieg < 60)
-        print("")
-    
-    
     personen = personen + einstieg
     ausstieg = input("Wie viele Personen steigen aus? ")
     ausstieg = int(ausstieg)
-    
-    
-    
     personen = personen - ausstieg
-    print("Es sind ",personen,"Personen im Bus.\n")
-
-
-
-
-
-
-
-
-
-
-
-
+    if (personen > 60):
+        print("Es sind zu viele Personen im Bus, Sie müssen",personen-60,"Personen da lassen!")
+        personen = 60
+    elif(personen < 0):
+        print("Es steigen mehr Personen ein als aus.Es sind nur",personen + ausstieg,"im Bus. Geben Sie nochmal ein!")
+        i = i-1
+    else:
+        print("Es sind ",personen,"Personen im Bus.\n")
 print("Der Bus hat die Endstation erreicht.")
 print("'Bitt alle aussteigen!'")
