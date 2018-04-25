f5f45ec572177dd668146aeaeb0f44d4e4a99ff2
Askcore <171witb14@o365.htl-leoben.at>
2018-04-25 08:59:37 +0200

    Updated version-Eingabe der Haltestellen

--- parkhaus.py
+++ parkhaus.py
@@ -1,2 +1,19 @@
 # parkhaus.py 
 # Angabe fuer das Beispiel: siehe Moodle
+
+print("Linienbus-Simulator")
+haltestellen = input("Wie viele Haltestellen soll es geben? ")
+
+einsteiger = input("Wie viele Personen wollen einsteigen? ")
+
+ 
+haltestellen = int(haltestellen)
+personen = int(einsteiger)
+
+
+
+
+
+
+print("Der Bus hat die Endstation erreicht.")
+print("Bitt alle aussteigen.")
